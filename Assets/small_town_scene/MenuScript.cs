b179d5a8e6387b3d1e76ea4975b68fda965f71af
jdeter14 <jdeter14@gmail.com>
2014-09-05 17:53:26 -0400

    Menu
    I made a placeholder menu which loads “Scene2”

---
+++ Assets/small_town_scene/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 84;
+		const int buttonHeight = 60;
+		
+		GUI.skin = skin;
+		
+		// Draw a button to start the game
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Start!"
+			)
+			)
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("scene2");
+		}
+	}
+}
