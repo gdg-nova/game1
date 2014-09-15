7e0661459be9ae588c597d863eacdc54258fa902
Howard <hdmassage@hotmail.com>
2014-09-15 09:12:40 -0400

    added a "Return to Menu" button on "scene8", added some torches, created a "template_scene" with 1 of each prefab and important items, turned off "human spawn" area

--- Assets/RestartScript.cs
+++ Assets/RestartScript.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Title screen script
 /// </summary>
-public class MenuScript : MonoBehaviour
+public class RestartScript : MonoBehaviour
 {
 	private GUISkin skin;
 	
@@ -14,8 +14,8 @@ public class MenuScript : MonoBehaviour
 	
 	void OnGUI()
 	{
-		const int buttonWidth = 84;
-		const int buttonHeight = 60;
+		const int buttonWidth = 120;
+		const int buttonHeight = 70;
 		
 		GUI.skin = skin;
 		
@@ -25,17 +25,17 @@ public class MenuScript : MonoBehaviour
 			// Center in X, 2/3 of the height in Y
 			new Rect(
 			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 3) - (buttonHeight / 2),
+			(2 * Screen.height / 14) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			),
-			"Start!"
+			"Return to Menu"
 			)
 			)
 		{
 			// On Click, load the first level.
 			// "Stage1" is the name of the first scene we created.
-			Application.LoadLevel("scene8");
+			Application.LoadLevel("menu");
 		}
 	}
 }
