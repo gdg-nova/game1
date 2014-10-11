2c67f90a6fc8b2e2da55855931efe564c1a9930a
Howard <hdmassage@hotmail.com>
2014-10-11 01:54:59 -0400

    no message

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -35,7 +35,7 @@ public class MenuScript : MonoBehaviour
 		{
 			// On Click, load the first level.
 			// "Stage1" is the name of the first scene we created.
-			Application.LoadLevel("scene8");
+			Application.LoadLevel("scene9");
 		}
 	}
 }
