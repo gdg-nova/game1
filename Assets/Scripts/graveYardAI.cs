66c092ebd2a12ada45fb16ba31f33dd71ea38427
mwbullen <mwbullen@mikes-mac-mini.home>
2014-10-31 08:00:08 -0400

    Zombie multiplier
    Optional modifier “ZombieMultiplier” on graveyard to spawn multiple
    zombies per mana point.  Also began adding Archer prefab (not
    functional yet)

--- Assets/Scripts/graveYardAI.cs
+++ Assets/Scripts/graveYardAI.cs
@@ -12,6 +12,8 @@ public class graveYardAI : MonoBehaviour
 	public float zombieSpawnInterval = 1.0f;
 	private float timeSinceLastSpawn;
 
+	public float zombieMultiplier = 1;
+
 	gameControl gameController = null;
 
 	public GameObject summonZombieEffect;
@@ -46,19 +48,37 @@ public class graveYardAI : MonoBehaviour
 
 		timeSinceLastSpawn = 0;
 
-		// time to generate a new zombie
-		float x = Random.Range(gameObject.collider.bounds.min.x, gameObject.collider.bounds.max.x);
-		float z = Random.Range(gameObject.collider.bounds.min.z, gameObject.collider.bounds.max.z);
 
 		Quaternion lightningAngle = Quaternion.Euler (-90, 0, 0);
 
-		Vector3 spawnLocation = new Vector3 (x, transform.position.y, z);
-		if (gameController.requestBuyNewZombie (spawnLocation)) {
-			GameObject lightning = (GameObject) GameObject.Instantiate(summonZombieEffect, spawnLocation,lightningAngle);
-			Destroy(lightning, 2f);
+		if (gameController.canIBuyAZombie()) {
+
+					// time to generate a new zombie
+				float x = Random.Range (gameObject.collider.bounds.min.x, gameObject.collider.bounds.max.x);
+				float z = Random.Range (gameObject.collider.bounds.min.z, gameObject.collider.bounds.max.z);
+
+				Vector3 spawnLocation = new Vector3 (x, transform.position.y, z);
+				if (gameController.requestBuyNewZombie (spawnLocation)) {
+					GameObject lightning = (GameObject)GameObject.Instantiate (summonZombieEffect, spawnLocation, lightningAngle);
+
+					Destroy (lightning, 2f);
+					}
+
+			//if multiplier is set, spawn additional free zombies
+			for (int i = 1; i <= zombieMultiplier; i++) {
+				float new_x = Random.Range (gameObject.collider.bounds.min.x, gameObject.collider.bounds.max.x);
+				float new_z = Random.Range (gameObject.collider.bounds.min.z, gameObject.collider.bounds.max.z);
+				
+				Vector3 new_spawnLocation = new Vector3 (new_x, transform.position.y, new_z);
+
+				gameController.createZombie (new_spawnLocation);
+				GameObject new_lightning = (GameObject)GameObject.Instantiate (summonZombieEffect, new_spawnLocation, lightningAngle);
+					
+				Destroy (new_lightning, 2f);
+				}
 			}
-			
-		timeSinceLastSpawn = 0;
+			timeSinceLastSpawn = 0;
+
 	}
 }
 
