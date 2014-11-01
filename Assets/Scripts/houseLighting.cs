055a68f1d7390ab96e82c818bcf911e705ef9415
DRapp <drapp@dvrss.com>
2014-11-01 10:11:42 -0400

    new shell to start handling house individual lighting.  Just getting proper handle on lighting objects into array and play with light/color/intensity.

---
+++ Assets/Scripts/houseLighting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class houseLighting : MonoBehaviour 
+{
+	Light[] houseLights;
+
+	// Use this for initialization
+	void Start () 
+	{
+		houseLights = gameObject.GetComponentsInChildren<Light>();
+		Debug.Log ( "House Light Count:" + houseLights.Length );
+		// default, turn off all the lights...
+//		foreach( Light hl in houseLights )
+//			hl.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach( Light hl in houseLights )
+		{
+//			hl.gameObject.SetActive(false);
+//			hl.intensity = .6f;
+//			hl.color = Color.white;
+			hl.color = new Color( 255f, 255f, 0f );
+//			hl.gameObject.SetActive(true);
+		}
+	}
+}
